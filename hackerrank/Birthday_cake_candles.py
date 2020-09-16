87265a105b92080303a397549766263d777bbb24
choiwonho <jsa1031109@naver.com>
2020-09-16 06:48:08 -0700

    2020.09.15 Python HackerRank commit

---
+++ hackerrank/Birthday_cake_candles.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    count_candles = Counter(candles).most_common(1)
+    print(count_candles)
+    max_candles = count_candles[0]
+    return max_candles[1]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
