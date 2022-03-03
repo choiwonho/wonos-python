89a1741cd0d0fcacab982fb0994b281930a76766
choiwonho <jsa1031109@naver.com>
2022-03-03 10:24:10 +0900

    Add number_16769.py by baekjoon

---
+++ backjoon/number_16769.py
@@ -0,0 +1,14 @@
+C, M = [0, 0, 0], [0, 0, 0]
+
+for i in range(3):
+	C[i], M[i] = map(int, input().split())
+
+for i in range(100):
+    now = i % 3
+    nxt = (i + 1) % 3
+
+    M[now], M[nxt] = max(M[now] - (C[nxt] - M[nxt]), 0), min(C[nxt], M[nxt] + M[now])
+
+
+for i in M:
+    print(i)
